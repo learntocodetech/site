89e2e12695f701741800b0862f912a181b39d33f
Lemuel <lemuel@Hans-Air.fios-router.home>
2019-04-03 13:23:02 -0400

    add blog and video sections, update section styles

---
+++ src/components/videoSection.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const VideoSection = () => {
+    return (<section class="video-section">
+                <div class="section-divider">
+                    <h3><span class="half">&lt</span>videos<span class="half">&gt;</span>
+                    </h3>
+                    </div>
+
+                    <div class="content">
+                    <div class="video-grid">
+                        <div class="video-item">
+                        <img src="https://img.youtube.com/vi/Yh7yOxy1ngw/hqdefault.jpg" />
+                        </div>
+                        
+                        <div class="video-item">
+                        <img src="https://img.youtube.com/vi/sEU12c-vG5U/hqdefault.jpg" />
+                        </div>
+                        
+                        <div class="video-item">
+                        <img src="https://img.youtube.com/vi/BK9d4gbLVnY/hqdefault.jpg" />
+                        </div>
+                        
+                        <div class="video-item">
+                        <img src="https://img.youtube.com/vi/u9tmrQaTSC0/hqdefault.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </section>)
+}
+
+export default VideoSection
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import { Link } from "gatsby"
 import Layout from "../components/shared/layout"
 import Hero from "../components/hero"
 import CodeSection from "../components/codeSection"
-// import BlogSection from "../components/blogSection"
-// import VideoSection from "../components/videoSection"
+import BlogSection from "../components/blogSection"
+import VideoSection from "../components/videoSection"
 // import ContactSection from "../components/contactSection"
 import SEO from "../components/shared/seo"
 
@@ -14,6 +14,8 @@ const IndexPage = () => {
             <SEO title="Learn to Code Tech" keywords={[`gatsby`, `application`, `react`]} />
             <Hero />
             <CodeSection />
+            <BlogSection />
+            <VideoSection />
           </Layout>)
 }
 
