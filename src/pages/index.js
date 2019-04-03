f9e2114cfdcd1f260c605aa0ab30e8e0de4ea0f4
Lemuel <lemuel@Hans-Air.fios-router.home>
2019-04-02 20:46:52 -0400

    add hero component and styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/shared/layout"
+import Hero from "../components/hero"
+// import CodeSection from "../components"
+// import BlogSection from "../components/blogSection"
+// import VideoSection from "../components/videoSection"
+// import ContactSection from "../components/contactSection"
 import SEO from "../components/shared/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-  </Layout>
-)
+const IndexPage = () => {
+  return (<Layout>
+            <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />]
+          </Layout>)
+}
 
 export default IndexPage
