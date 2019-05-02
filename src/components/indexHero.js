4e9e1d2e6f88783413cedf826f268bf0d1d6e84a
Lemuel <lemuel@Hans-MacBook-Air.local>
2019-05-02 09:04:15 -0400

    update hero styles

--- src/components/indexHero.js
+++ src/components/indexHero.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Hero from './hero'
+import JSIcon from '../images/javascript.svg'
 
 const IndexHero = () => {
     return (<Hero>
                 <div className="index-hero">
                     <h1 className="index-hero__title">LEARN TO <span className="color">CODE</span> FOR FREE</h1>
+                    {/* <img src={JSIcon} /> */}
                 </div>
             </Hero>)
 }
 
 export default IndexHero
+
