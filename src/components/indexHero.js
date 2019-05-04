d0eeca69afd2b7cd33e5d9bda222f4c16f9a174a
Lemuel <lemuel@Hans-Air.fios-router.home>
2019-05-04 12:00:07 -0400

    update hero styles

--- src/components/indexHero.js
+++ src/components/indexHero.js
@@ -6,10 +6,9 @@ const IndexHero = () => {
     return (<Hero>
                 <div className="index-hero">
                     <h1 className="index-hero__title">
-                        <span className="color">Conquer</span> Algorithms<br/>
-                        Learn to <span className="color">Code</span>
+                        <span className="color">Conquer</span> Algorithms &<br/>
+                        Make Amazing <span className="color">Chatbots</span>
                     </h1>
-                    {/* <img src={JSIcon} /> */}
                 </div>
             </Hero>)
 }
